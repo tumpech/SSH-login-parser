b5d440dedd97d447ec80cb1d951a7b97c17f78fb
tumpech <tumpech@users.noreply.github.com>
2014-04-18 17:21:09 +0200

    Added q as command.
    Now you can quit if it found more entry with q.

--- conn.sh
+++ conn.sh
@@ -3,6 +3,7 @@ function c {
 declare -a cn=("${!1}")
 read ch
 case $ch in
+q) echo Quitting!! && exit;;
 ''|*[!0-9]*) echo "Your choice is not a number." && c $1;;
 *)if [ $ch -lt ${#cn[*]} ]
 then
@@ -21,12 +22,13 @@ IFS=$'\n'
 conn=($(egrep -i ssh.*$1 $c_file))
 unset IFS
 case ${#conn[*]} in
-        0) echo "No luck my master, try again." && exit;;
+        0) echo "No luck my lord, try again." && exit;;
         1) echo "Connecting" &&  eval $conn;;
         *)
                 "Found these:"
                 for i in ${!conn[*]}; do
                         echo $i")" ${conn[$i]}; done
+                echo "q) quitting from the script"
                 echo "Please make your choice: "
                 c conn[@];
 esac
