88744e6d313963f516f786da3f26333838a88f6d
tumpech <tumpech@users.noreply.github.com>
2014-04-28 16:14:27 +0200

    New functionality
    Conn script updated. Now it is working with user@host connections from the file, meaning there is much more space to personalize the log in to the server as a whole.

--- conn.sh
+++ conn.sh
@@ -7,28 +7,31 @@ q) echo Quitting!! && exit;;
 ''|*[!0-9]*) echo "Your choice is not a number." && c $1;;
 *)if [ $ch -lt ${#cn[*]} ]
 then
-        echo ${cn[$ch]}
-        eval ${cn[$ch]}
+	clear
+	echo ${cn[$ch]}
+	eval "ssh -X ${cn[$ch]}"
+	clear
 else
-        echo "Well, I wrote everything down from 0 to $[${#cn[*]} - 1] please write between those."
-        c $1
+	echo "Well, I wrote everything down from 0 to $[${#cn[*]} - 1] please write between those."
+	c $1
 fi;;
 esac
 }
 
+clear
 c_file=connections.txt
 echo "Searching for $1"
 IFS=$'\n'
-conn=($(egrep -i ssh.*$1 $c_file))
+conn=($(grep -i $1 $c_file | grep -v "#"))
 unset IFS
 case ${#conn[*]} in
-        0) echo "No luck my lord, try again." && exit;;
-        1) echo "Connecting" &&  eval $conn;;
-        *)
-                "Found these:"
-                for i in ${!conn[*]}; do
-                        echo $i")" ${conn[$i]}; done
-                echo "q) quitting from the script"
-                echo "Please make your choice: "
-                c conn[@];
+	0) echo "No luck my lord, try again." && exit;;
+	1) echo "Connecting" &&  eval "ssh -X $conn" && clear;;
+	*)
+		echo "Found these:"
+		for i in ${!conn[*]}; do
+			echo $i")" ${conn[$i]};	done
+		echo "q) quitting from the script"
+		echo "Please make your choice: "
+		c conn[@];
 esac
