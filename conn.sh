f4929279c1ce75dc260d419c64899b207e7d5e6d
tumpech <tumpech@users.noreply.github.com>
2014-11-30 20:01:39 +0100

    Update conn.sh
    v2.0
    * Added a new function to check from groups (line starting with #)
    * Made the code simpler
    * Added a new function to use an ssh-agent on the computer.

--- conn.sh
+++ conn.sh
@@ -1,37 +1,52 @@
 #!/bin/bash
 function c {
-declare -a cn=("${!1}")
-read ch
-case $ch in
-q) echo Quitting!! && exit;;
-''|*[!0-9]*) echo "Your choice is not a number." && c $1;;
-*)if [ $ch -lt ${#cn[*]} ]
-then
-	clear
-	echo ${cn[$ch]}
-	eval "ssh -X ${cn[$ch]}"
-	clear
-else
-	echo "Well, I wrote everything down from 0 to $[${#cn[*]} - 1] please write between those."
-	c $1
-fi;;
-esac
+declare -a cn=("${!1}");
+PS3="Please make your choice:"
+
+echo "Found these:"
+select ch in "${cn[@]}" "quit" ;
+do case $ch in
+                '')echo "No choice!"; continue;;
+                'quit') echo "Bye :)" && exit;;
+                *)local  resultvar=$2; eval $resultvar="'$ch'"; break;;
+esac;done
 }
 
+function apc {
+#read-file
+bs=($(egrep -i "#.*$1" $c_file)) #name of matches
+case ${#bs[*]} in #check match number
+        0) echo "No luck my lord, try again." && exit;; #0=exit
+        1) bn=$(egrep -in "$bs" $c_file | awk -F":" '{print $1}');; #1=connect
+        *) c bs[@] ch; bn=$(egrep -in "^$ch$" $c_file | awk -F":" '{print $1}');; #more=choose/connect
+esac
+en=$(cat $c_file | awk "NR > $bn" | egrep -inm 1 "#" | awk -F":" '{print $1}') #sorting out specific connections
+let en+=$bn
+if [ "$en" -eq "$bn" ]; then
+conn=($(cat $c_file | awk "NR > $bn"))
+else
+conn=($(cat $c_file | awk "NR > $bn && NR < $en"))
+fi
+}
+
+. ssh-agent.sh #for automatic ssh-agent
 clear
-c_file=connections.txt
-echo "Searching for $1"
-IFS=$'\n'
-conn=($(grep -i $1 $c_file | grep -v "#"))
+c_file=connections.txt #source file
+
+if [ ! -f $c_file ]; then #source file check
+ echo "$c_file not founs"; exit
+fi
+
+IFS=$'\n' #read-file
+case $1 in
+        "")echo "Searching for $1"; conn=($(grep -v "#" $c_file));; #incase there is no parameter
+        -a)apc $2;; #read applications
+        *)echo "Searching for $1"; conn=($(grep -v "#" $c_file | grep -i $1));; #read connections
+esac
 unset IFS
-case ${#conn[*]} in
-	0) echo "No luck my lord, try again." && exit;;
-	1) echo "Connecting" &&  eval "ssh -X $conn" && clear;;
-	*)
-		echo "Found these:"
-		for i in ${!conn[*]}; do
-			echo $i")" ${conn[$i]};	done
-		echo "q) quitting from the script"
-		echo "Please make your choice: "
-		c conn[@];
+
+case ${#conn[*]} in #check match number
+        0) echo "No luck my lord, try again." && exit;; #0=exit
+        1) echo -e "Connecting to $conn \n" &&  eval "ssh -X $conn" && clear;; #1=connect
+        *) c conn[@] ch; echo -e "Connecting to $ch\n"; eval "ssh -X $ch";; #more=choose/connect
 esac
